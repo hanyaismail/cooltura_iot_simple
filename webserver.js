d37a122440fc97f75b8dbcb26239003cc20b14a7
Ismail <ismailkasep@gmail.com>
2018-12-05 23:16:52 +0700

    add onoff gpio depedency

--- webserver.js
+++ webserver.js
@@ -1,6 +1,15 @@
 const http = require('http').createServer(handler);
 const fs = require('fs')
 const io = require('socket.io')(http)
+const Gpio = require('onoff').Gpio
+
+const fogger1 = new Gpio(4, 'out')
+const fogger2 = new Gpio(5, 'out')
+const fogger3 = new Gpio(6, 'out')
+
+const light1 = new Gpio(7, 'out')
+const light2 = new Gpio(8, 'out')
+const light3 = new Gpio(9, 'out')
 
 http.listen(8080, () => {
   console.log('listening on port 8080')
@@ -19,14 +28,34 @@ function handler(req, res) {
 }
 
 io.on('connection', socket => {
-  socket.emit('news', {hello: 'world'});
-  socket.on('myOtherEvent', data => {
-    console.log('data', data);
-  })
   socket.on('light', data => {
     console.log('light data', data)
+    lightValue = data.state
+    switch (data.lightNum) {
+      case 1:
+        light1.writeSync(lightValue);
+        break;
+      case 2:
+        light2.writeSync(lightValue);
+        break;
+      case 3:
+        light3.writeSync(lightValue);
+        break;
+    }
   })
   socket.on('fogger', data => {
     console.log('fogger data', data)
+    foggerValue = data.state
+    switch (data.lightNum) {
+      case 1:
+        fogger1.writeSync(foggerValue);
+        break;
+      case 2:
+        fogger2.writeSync(foggerValue);
+        break;
+      case 3:
+        fogger3.writeSync(foggerValue);
+        break;
+    }
   })
 })
